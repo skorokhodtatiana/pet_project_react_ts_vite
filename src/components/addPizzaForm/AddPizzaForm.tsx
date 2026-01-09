dddcb980d0dae603d537640a83e634be5e2f4d7d
tskorohod <tskorohod@lmteam.by>
2026-01-09 20:36:16 +0300

    added component form for added new Pizza

---
+++ src/components/addPizzaForm/AddPizzaForm.tsx
@@ -0,0 +1,76 @@
+import { FC, useState, ChangeEvent, FormEvent } from 'react';
+
+import './styles.css';
+import Pizza from '../../models/Pizza';
+
+const initialState = {
+		title: '',
+		price: '',
+		img: ''
+	}
+
+	interface addPizzaFormProps {
+		addNewPizza: (newPizza:Pizza) => void
+	};
+
+const AddPizzaForm: FC<addPizzaFormProps> = ({addNewPizza}) => {
+	const [newPizza, setNewPizza] = useState<{title: string, price: string, img: string}>(initialState);
+
+	const handleClick = (e:ChangeEvent<HTMLInputElement>) => {
+		const {name, value} = e.target;
+		setNewPizza({
+			...newPizza,
+			[name]: value,
+
+		})
+	}
+
+	const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const {title, price, img} = newPizza;
+
+		if (title && img && price) {
+			addNewPizza({
+				title,
+				price,
+				img,
+				id: Date.now()
+			})
+			setNewPizza(initialState);
+			console.log(newPizza)
+		}
+	}
+
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<input
+					name="title"
+					type="text"
+					placeholder="Название"
+					onChange={handleClick}
+					value={newPizza.title}
+				/>
+				<input
+					name="price"
+					type="text"
+					placeholder="Стоимость"
+					onChange={handleClick}
+					value={newPizza.price}
+				/>
+				<input
+					name="img"
+					type="text"
+					placeholder="Изображение"
+					onChange={handleClick}
+					value={newPizza.img}
+				/>
+				<button type="submit">
+					Добавить в меню
+				</button>
+			</form>
+		</>
+	)
+};
+
+export default AddPizzaForm;
