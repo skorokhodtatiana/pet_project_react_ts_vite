46031b2c2187def3cc3ef535cf7f664a2bc469bb
tskorohod <tskorohod@lmteam.by>
2026-01-12 18:29:37 +0300

    added icons

--- src/components/singlePizza/SinglePizza.tsx
+++ src/components/singlePizza/SinglePizza.tsx
@@ -1,19 +1,25 @@
 import { FC } from "react";
-
-import './singlePizza.css';
 import Pizza from "../../models/Pizza";
+import { AiFillEdit } from "react-icons/ai";
+import { AiFillDelete } from "react-icons/ai";
+
+import './singlePizza.scss';
+
 
 interface singlePizzaProps {
 	pizza: Pizza
 }
 
-
 const SinglePizza:FC<singlePizzaProps> = ({pizza}) => {
 	return (
 		<div className="pizza">
-			<img src={`src/assets/images/${pizza.img}`} alt={pizza.img}/>
-			<h2>{pizza.title}</h2>
-			<span className="price">{pizza.price}</span>
+			<img className="pizza__img" src={`src/assets/images/${pizza.img}`} alt={pizza.img}/>
+			<h2 className="pizza__title">{pizza.title}</h2>
+			<span className="pizza__price">{pizza.price}</span>
+			<div className="pizza__controls">
+				<AiFillEdit></AiFillEdit>
+				<AiFillDelete></AiFillDelete>
+			</div>
 		</div>
 	)
 }
